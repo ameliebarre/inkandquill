a4e064d15db2d11faa788545c8765c77962c4d19
Amelie Barre <amelie.barre@healthhero.com>
2024-12-06 20:15:26 +0100

    implements and design header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,37 +9,21 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        dark: "#2C2C2C",
+        gold: "#FFD700",
+      },
       fontFamily: {
         sans: ["var(--font-dm-sans)"],
         serif: ["var(--font-dm-serif)"],
       },
     },
     fontSize: {
-      "2xl": [
-        "1.5rem",
-        {
-          lineHeight: "2rem",
-          letterSpacing: "-0.01em",
-          fontWeight: "500",
-        },
-      ],
-      "3xl": [
-        "1.875rem",
-        {
-          lineHeight: "2.25rem",
-          letterSpacing: "-0.02em",
-          fontWeight: "700",
-        },
-      ],
       sm: ["14px", "20px"],
       base: ["16px", "24px"],
       lg: ["20px", "28px"],
       xl: ["24px", "32px"],
-    },
-    colors: {
-      white: "#FFFFFF",
-      dark: "#2C2C2C",
-      gold: "#FFD700",
+      "2xl": ["32px", "40px"],
     },
   },
   plugins: [],
